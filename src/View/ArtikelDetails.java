d68e940ec6450b3845e02384a84be8b8f1598f76
jackderblaueaffe <ch.fix@gmx.de>
2015-05-17 01:50:11 +0200

    cfix Zwischenstand 17.5.15
    Warenkorb erweitert

--- src/View/ArtikelDetails.java
+++ src/View/ArtikelDetails.java
@@ -22,10 +22,9 @@ public class ArtikelDetails extends javax.swing.JFrame {
         
         setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
         this.artikel = artikel;
-        //System.out.println(artikel.getName());
         this.LB_ArtikelName.setText(artikel.getName());
         this.LB_ArtikelBeschreibung.setText(artikel.getBeschreibung());
-        //this.LB_ArtikelPreis.setText(artikel.g);
+        this.LB_ArtikelPreis.setText(String.valueOf((double)((int)((artikel.getMehrwertsteuer() * artikel.getNettopreis()) / 100 ) / 100d)) + " €"); //zwei Nachkommstellen
     }
 
     /**
@@ -40,16 +39,19 @@ public class ArtikelDetails extends javax.swing.JFrame {
         LB_ArtikelName = new javax.swing.JLabel();
         LB_ArtikelBeschreibung = new javax.swing.JLabel();
         TB_ArtikelMenge = new javax.swing.JTextField();
-        LB_ArtikelMenge = new javax.swing.JLabel();
         LB_ArtikelPreis = new javax.swing.JLabel();
         BT_Warenkorb = new javax.swing.JButton();
+        LB_X = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        LB_ArtikelName.setText("ArtikelName");
+        LB_ArtikelName.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        LB_ArtikelName.setText("Artikelname");
 
-        LB_ArtikelBeschreibung.setText("ArtikelBeschreibung");
+        LB_ArtikelBeschreibung.setText("Artikelbeschreibung");
 
+        TB_ArtikelMenge.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        TB_ArtikelMenge.setText("1");
         TB_ArtikelMenge.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 TB_ArtikelMengeActionPerformed(evt);
@@ -63,16 +65,16 @@ public class ArtikelDetails extends javax.swing.JFrame {
             }
         });
 
-        LB_ArtikelMenge.setText("Menge");
-
         LB_ArtikelPreis.setText("Preis");
 
-        BT_Warenkorb.setText("In Warenkorb");
+        BT_Warenkorb.setText("In den Warenkorb");
         BT_Warenkorb.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BT_WarenkorbActionPerformed(evt);
             }
         });
+
+        LB_X.setText("x");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -85,13 +87,13 @@ public class ArtikelDetails extends javax.swing.JFrame {
                         .addComponent(BT_Warenkorb)
                         .addComponent(LB_ArtikelBeschreibung)
                         .addComponent(LB_ArtikelName))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(LB_ArtikelMenge)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addComponent(TB_ArtikelMenge, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(29, 29, 29)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(LB_X)
+                        .addGap(18, 18, 18)
                         .addComponent(LB_ArtikelPreis)))
-                .addContainerGap(189, Short.MAX_VALUE))
+                .addContainerGap(188, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -103,11 +105,11 @@ public class ArtikelDetails extends javax.swing.JFrame {
                 .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(TB_ArtikelMenge, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(LB_ArtikelMenge)
-                    .addComponent(LB_ArtikelPreis))
+                    .addComponent(LB_ArtikelPreis)
+                    .addComponent(LB_X))
                 .addGap(37, 37, 37)
                 .addComponent(BT_Warenkorb)
-                .addContainerGap(59, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
 
         pack();
@@ -148,9 +150,9 @@ public class ArtikelDetails extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BT_Warenkorb;
     private javax.swing.JLabel LB_ArtikelBeschreibung;
-    private javax.swing.JLabel LB_ArtikelMenge;
     private javax.swing.JLabel LB_ArtikelName;
     private javax.swing.JLabel LB_ArtikelPreis;
+    private javax.swing.JLabel LB_X;
     private javax.swing.JTextField TB_ArtikelMenge;
     // End of variables declaration//GEN-END:variables
 }
